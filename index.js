d686eca95e53b8a6aa69c7e38b6562348487fcdd
Fabio Souza <fabiohrsouza@gmail.com>
2023-05-20 12:27:08 -0300

    feat: exercicios 1 e 2

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http")
+const fs = require("fs")
+const querystring = require("querystring")
+
+
+
+const server = http.createServer((req, res) => {
+
+  
+})
+
+
+server.listen(3000)
+    console.log("Servidor rodando na porta http://localhost:3000")
+    
