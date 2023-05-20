2b917cb8ff4c0bef0f3383b47d33a1657d913a1c
Fabio Souza <fabiohrsouza@gmail.com>
2023-05-20 12:41:15 -0300

    feat: exercicio 5

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const server = http.createServer((req, res) => {
         switch(req.method){
           
             case "GET":
-            // aqui vai a lógica do GET - exercício 5
+                response.writeHead(200, {"Content-Type": "application/json; charset: utf-8;"})
+                response.end(ListarProdutos())
             break
 
             case "POST":
